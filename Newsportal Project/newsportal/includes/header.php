60bef0c106b1f5832f7ab574bba795fe495e871e
Hevindu Sasnitha <hevindusekanayake@gmail.com>
2021-10-05 02:52:01 +0530

    Website test

---
+++ Newsportal Project/newsportal/includes/header.php
@@ -0,0 +1,54 @@
+ <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="index.php"><img src="logo.png" height="50"></a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="about-us.php">HOME</a>
+            </li>
+                  <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownPortfolio" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                CARS
+              </a>
+              <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownPortfolio">
+                <a class="dropdown-item" href="portfolio-1-col.html">SUPERCARS</a>
+                <a class="dropdown-item" href="portfolio-2-col.html">OFF ROAD</a>
+                <a class="dropdown-item" href="portfolio-3-col.html">CLASSICS</a>
+                <a class="dropdown-item" href="portfolio-4-col.html">EXOTICS</a>
+                <a class="dropdown-item" href="portfolio-item.html">EVs</a>
+                <a class="dropdown-item" href="portfolio-item.html">HYBRID</a>
+             
+  
+             <li class="nav-item">
+              <a class="nav-link" href="contact-us.php">CAR MANUALS</a>
+            </li>
+             </li>
+             <li class="nav-item">
+              <a class="nav-link" href="contact-us.php">PROMO</a>
+            </li>
+	    </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownPortfolio" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                MORE
+              </a>
+              <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownPortfolio">
+                <a class="dropdown-item" href="portfolio-1-col.html">CAR FACTS</a>
+                <a class="dropdown-item" href="portfolio-2-col.html">CAR MEMES</a>
+                <a class="dropdown-item" href="portfolio-3-col.html">QUIZZES</a>
+                <a class="dropdown-item" href="portfolio-4-col.html">NEWS ROOM</a>
+                <a class="dropdown-item" href="portfolio-item.html">FORMULA FANS</a>
+                <a class="dropdown-item" href="portfolio-item.html">GAMING</a>
+                <a class="dropdown-item" href="portfolio-item.html">JDM FANS</a>
+                <a class="dropdown-item" href="portfolio-item.html">SPOTTINGS</a>
+                <a class="dropdown-item" href="portfolio-item.html">RETRO</a>
+                <a class="dropdown-item" href="portfolio-item.html">REVIEWS</a>
+                <a class="dropdown-item" href="portfolio-item.html">EVENTS</a>
+  
+  
+          </ul>
+        </div>
+      </div>
+    </nav>
---
+++ Newsportal Project/newsportal/index.php
@@ -0,0 +1,238 @@
+<?php 
+session_start();
+include('includes/config.php');
+
+    ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>News Portal | Home Page</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/modern-business.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+    <!-- Navigation -->
+   <?php include('includes/header.php');?>
+
+    <!-- Page Content -->
+    <div class="container">
+
+
+     
+      <div class="row" style="margin-top: 4%">
+
+        <!-- Blog Entries Column -->
+        <div class="col-md-8">
+
+          <!-- Blog Post -->
+<?php 
+     if (isset($_GET['pageno'])) {
+            $pageno = $_GET['pageno'];
+        } else {
+            $pageno = 1;
+        }
+        $no_of_records_per_page = 8;
+        $offset = ($pageno-1) * $no_of_records_per_page;
+
+
+        $total_pages_sql = "SELECT COUNT(*) FROM tblposts";
+        $result = mysqli_query($con,$total_pages_sql);
+        $total_rows = mysqli_fetch_array($result)[0];
+        $total_pages = ceil($total_rows / $no_of_records_per_page);
+
+
+$query=mysqli_query($con,"select tblposts.id as pid,tblposts.PostTitle as posttitle,tblposts.PostImage,tblcategory.CategoryName as category,tblcategory.id as cid,tblsubcategory.Subcategory as subcategory,tblposts.PostDetails as postdetails,tblposts.PostingDate as postingdate,tblposts.PostUrl as url from tblposts left join tblcategory on tblcategory.id=tblposts.CategoryId left join  tblsubcategory on  tblsubcategory.SubCategoryId=tblposts.SubCategoryId where tblposts.Is_Active=1 order by tblposts.id desc  LIMIT $offset, $no_of_records_per_page");
+while ($row=mysqli_fetch_array($query)) {
+?>
+
+          <div class="card mb-4">
+ <img class="card-img-top" src="admin/postimages/<?php echo htmlentities($row['PostImage']);?>" alt="<?php echo htmlentities($row['posttitle']);?>">
+            <div class="card-body">
+              <h2 class="card-title"><?php echo htmlentities($row['posttitle']);?></h2>
+                 <p><b>Category : </b> <a href="category.php?catid=<?php echo htmlentities($row['cid'])?>"><?php echo htmlentities($row['category']);?></a> </p>
+       
+              <a href="news-details.php?nid=<?php echo htmlentities($row['pid'])?>" class="btn btn-primary">Read More &rarr;</a>
+            </div>
+            <div class="card-footer text-muted">
+              Posted on <?php echo htmlentities($row['postingdate']);?>
+           
+            </div>
+          </div>
+<?php } ?>
+       
+
+      
+
+          <!-- Pagination -->
+
+
+    <ul class="pagination justify-content-center mb-4">
+        <li class="page-item"><a href="?pageno=1"  class="page-link">First</a></li>
+        <li class="<?php if($pageno <= 1){ echo 'disabled'; } ?> page-item">
+            <a href="<?php if($pageno <= 1){ echo '#'; } else { echo "?pageno=".($pageno - 1); } ?>" class="page-link">Prev</a>
+        </li>
+        <li class="<?php if($pageno >= $total_pages){ echo 'disabled'; } ?> page-item">
+            <a href="<?php if($pageno >= $total_pages){ echo '#'; } else { echo "?pageno=".($pageno + 1); } ?> " class="page-link">Next</a>
+        </li>
+        <li class="page-item"><a href="?pageno=<?php echo $total_pages; ?>" class="page-link">Last</a></li>
+    </ul>
+
+        </div>
+
+        <!-- Sidebar Widgets Column -->
+      <?php include('includes/sidebar.php');?>
+      </div>
+      <!-- /.row -->
+
+    </div>
+    <!-- /.container -->
+
+    <!-- Footer -->
+       <link href="51.bootstrape footer/style.css" rel="stylesheet">
+
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+     <!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Footer Template</title>
+
+    <!-- Google Fonts Cdn -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;400;600;700;800;900&display=swap"
+        rel="stylesheet">
+
+    <!-- Font Awesome Cdn -->
+    <script src="https://kit.fontawesome.com/e48d166edc.js" crossorigin="anonymous"></script>
+
+    <!-- Bootstrap Cdn -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <!-- Main Css -->
+    <link rel="stylesheet" href="style.css">
+
+
+</head>
+
+<body>
+    <!-- Footer section Begin -->
+    <footer class="footer">
+        <div class="container">
+            <div class="row">
+
+
+                <div class="col-lg-3 col-md-6 col-sm-6">
+                    <div class="footer-about">
+                        <div class="footer-logo">
+                            <a href="#"><img src="51.bootstrape footer\img/Unique Cars SL logo.jpg" height="100" alt="index.php"></a>
+                        </div>
+                        <p>Unique Cars SL™</p>
+                        <a href="#"><img src="img/payment.png" alt=""></a>
+                    </div>
+                </div>
+				
+                
+                <div class="col-lg-2 offset-lg-1 col-md-3 col-sm-6">
+                    <div class="footer-widget">
+                        <h6></h6>
+                        <ul>
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">Advertising</a></li>
+                            <li><a href="#">Copyright Notice</a></li>
+                            <li><a href="#">Disclaimer</a></li>
+                             <li><a href="#">Privacy Policy</a></li>
+							  <li><a href="#">Contact Us</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="col-lg-2  col-md-3 col-sm-6">
+                    <div class="footer-widget">
+                        <h6>Links</h6>
+                        <ul>
+                            <li><a href="#">Contact Us</a></li>
+                            <li><a href="#">Payment Methods</a></li>
+                            <li><a href="#">Delivary</a></li>
+                            <li><a href="#">Return & Exchanges</a></li>
+
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 offset-lg-1 col-md-6 col-sm-6">
+                    <div class="footer-widget">
+                        <h6>NewsLetter</h6>
+                        <div class="footer-newslatter">
+                            <p>Be the first to know about new arrivals, look books, sales & promos!</p>
+                            <form action="#">
+                                <input type="text" placeholder="Your Email">
+                                <button type="submit"><span><i class="fa fa-envelope"
+                                            arial-hidden="true"></i></span></button>
+                            </form>
+                        </div>
+
+                    </div>
+					<link rel="Newsportal Project\newsportal\follow us" href="styles123.css">
+					<ul class="social_link">
+                                        <li><a href="#"><img src="icon/fb.png"></a></li>
+                                        <li><a href="#"><img src="icon/tw.png"></a></li>
+                                        <li><a href="#"><img src="icon/lin(2).png"></a></li>
+                                         <li><a href="#"><img src="icon/instagram.png"></a></li>
+                                    </ul>
+				 
+                  
+                </div>
+
+
+            </div>
+
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <div class="footer-copyright-text">
+                        <p>Copyright &copy; 2021 All rights reserved Unique Cars SL™ </i>
+                             </p>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+
+    </footer>
+
+
+
+    <!-- Bootstrap Js Cdn -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
+        integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF"
+        crossorigin="anonymous"></script>
+
+</body>
+
+</html>
+ 
+</head>
+  </body>
+
+</html>
